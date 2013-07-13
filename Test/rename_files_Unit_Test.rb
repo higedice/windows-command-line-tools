a177e1b327134a9d699f3ee3af0fb01fd34379ae
HigeDice <higedice@banjoyugi.net>
2013-07-13 11:28:03 +0900

    Add rename_files.rb and It's my first Ruby App.

---
+++ Test/rename_files_Unit_Test.rb
@@ -0,0 +1,99 @@
+require 'FileUtils'
+
+`rm -rf test-tmp-*`
+
+
+
+
+
+
+FileUtils.touch "test-tmp-abcabc.txt"
+FileUtils.touch "test-tmp-abcdef.txt"
+FileUtils.touch "test-tmp-あいうえお.txt"
+FileUtils.touch "test-tmp-あああああ.txt"
+FileUtils.touch "test-tmp-abc123abc456x.txt"
+
+Dir.mkdir "test-tmp-abc"
+Dir.mkdir "test-tmp-d1"
+
+Dir.chdir "test-tmp-d1"
+FileUtils.touch "test-tmp-abcabc.txt"
+Dir.chdir ".."
+
+
+
+puts `ruby rename_files.rb abc ABC . test-tmp-d1/`
+
+if FileTest.exists? "test-tmp-ABCABC.txt" then
+	puts "Test 0001-01: OK"
+else
+	puts "Test 0001-01: NG"
+end
+
+if FileTest.exists? "test-tmp-ABCdef.txt" then
+	puts "Test 0001-02: OK"
+else
+	puts "Test 0001-02: NG"
+end
+
+
+if FileTest.exists? "test-tmp-ABC123ABC456x.txt" then
+	puts "Test 0001-03: OK"
+else
+	puts "Test 0001-03: NG"
+end
+
+
+
+if FileTest.exists? "test-tmp-abc" then
+	puts "Test 0001-04: OK"
+else
+	puts "Test 0001-04: NG"
+end
+
+
+if FileTest.exists? "test-tmp-d1/test-tmp-ABCABC.txt" then
+	puts "Test 0001-05: OK"
+else
+	puts "Test 0001-05: NG"
+end
+
+
+s = 'ruby rename_files.rb "あ{3}" a . '.encode("Shift_JIS")
+puts `#{s}`
+
+
+if FileTest.exists? "test-tmp-aああ.txt" then
+	puts "Test 0002-01: OK"
+else
+	puts "Test 0002-01: NG or NOT Windows"
+end
+
+
+
+
+s = 'ruby rename_files.rb "あ" b "test-tmp-あいうえお.txt"'.encode("Shift_JIS")
+puts `#{s}`
+
+
+if FileTest.exists? "test-tmp-aああ.txt" then
+	puts "Test 0003-01: OK"
+else
+	puts "Test 0003-01: NG or NOT Windows"
+end
+
+
+if FileTest.exists? "test-tmp-bいうえお.txt" then
+	puts "Test 0003-02: OK"
+else
+	puts "Test 0003-02: NG or NOT Windows"
+end
+
+
+
+puts `ruby rename_files.rb d1 d2 test-tmp-d1`
+
+
+
+
+
